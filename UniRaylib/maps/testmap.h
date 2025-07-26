708b063cfa4c4ab36be15e01680ec94c029749fe
feive7 <feive7@gmail.com>
2025-07-26 09:41:57 -0700

    Map change

--- UniRaylib/maps/testmap.h
+++ UniRaylib/maps/testmap.h
@@ -6,12 +6,11 @@ GameMap testmap = {
 		{1.0f,2.0f,0.0f},
 	},
 	{ // Walls
-		{{{-10.0f,10.0f},{10.0f,10.0f},{10.0f,-10.0f},{-10.0f,-10.0f}},-2.0f,2.0f,&tex_stone,WHITE,&shader_lighting,0}, // stone.025
-		{{{7.04f,-9.69f},{-7.04f,-9.69f},{-5.87f,-8.08f},{5.87f,-8.08f}},0.0f,10.0f,&tex_stone,WHITE,&shader_lighting,0}, // stone.026
-		{{{-7.04f,-9.69f},{-11.4f,3.7f},{-9.49f,3.08f},{-5.87f,-8.08f}},0.0f,10.0f,&tex_stone,WHITE,&shader_lighting,0}, // stone.027
-		{{{-11.4f,3.7f},{-0.0f,12.0f},{-0.0f,10.0f},{-9.49f,3.08f}},0.0f,10.0f,&tex_stone,WHITE,&shader_lighting,0}, // stone.028
-		{{{-0.0f,12.0f},{11.39f,3.7f},{9.49f,3.08f},{-0.0f,10.0f}},0.0f,10.0f,&tex_stone,WHITE,&shader_lighting,0}, // stone.029
-		{{{11.39f,3.7f},{7.04f,-9.69f},{5.87f,-8.08f},{9.49f,3.08f}},0.0f,10.0f,&tex_stone,WHITE,&shader_lighting,0}, // stone.030
+		{{{-0.24f,9.33f},{11.98f,4.89f},{11.3f,3.01f},{-0.92f,7.45f}},0.0f,12.0f,&tex_rockwall,WHITE,&shader_lighting,0}, // rockwall
+		{{{-11.98f,4.89f},{0.24f,9.33f},{0.92f,7.45f},{-11.3f,3.01f}},0.0f,12.0f,&tex_rockwall,WHITE,&shader_lighting,0}, // rockwall.013
+		{{{-12.0f,-10.0f},{-12.0f,5.0f},{-10.0f,5.0f},{-10.0f,-10.0f}},0.0f,12.0f,&tex_rockwall,WHITE,&shader_lighting,0}, // rockwall.014
+		{{{10.0f,-10.0f},{10.0f,5.0f},{12.0f,5.0f},{12.0f,-10.0f}},0.0f,12.0f,&tex_rockwall,WHITE,&shader_lighting,0}, // rockwall.015
+		{{{-10.0f,10.0f},{10.0f,10.0f},{10.0f,-10.0f},{-10.0f,-10.0f}},-2.0f,2.0f,&tex_stone,WHITE,&shader_lighting,0}, // stone.031
 	},
 	{ // Signs (WIP)
 	},
@@ -19,6 +18,6 @@ GameMap testmap = {
 	},
 	0.0f, // Ambient lighting
  	{ // Lights
-		{{0.0f,8.0f,0.0f}, 28.41f, {1.0f,1.0f,1.0f}},
+		{{0.0f,7.0f,0.0f}, 66.31f, {1.0f,1.0f,1.0f}},
 	},
 };
