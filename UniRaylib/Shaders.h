577ce8a247ac8da4df31051f7ee5871bef4534ee
feive7 <feive7@gmail.com>
2025-07-29 11:06:17 -0700

    added none shader

--- UniRaylib/Shaders.h
+++ UniRaylib/Shaders.h
@@ -1,11 +1,13 @@
+Shader shader_none;
 Shader shader_test;
 Shader shader_window;
 Shader shader_lighting;
 
 int current_shader = 0;
-Shader* shaders[] = {&shader_lighting,&shader_test};
+Shader* shaders[] = {&shader_none,&shader_lighting,&shader_test};
 const int shader_count = sizeof(shaders) / sizeof(Shader*);
 void InitShaders() {
+	shader_none = LoadShader("assets/shaders/base.vs", "assets/shaders/base.fs");
 	shader_test = LoadShader("assets/shaders/test.vs", "assets/shaders/test.fs");
 	shader_window = LoadShader("assets/shaders/window.vs", "assets/shaders/window.fs");
 	shader_lighting = LoadShader("assets/shaders/lighting.vs", "assets/shaders/lighting2.fs");
