dcd1f6edda8f18d7b9c18e6f437e3dd4303eba7c
feive7 <feive7@gmail.com>
2025-07-29 11:22:32 -0700

    Make sure ball starts with the current shader

--- UniRaylib/Models.h
+++ UniRaylib/Models.h
@@ -1,6 +1,6 @@
 Model mdl_ball;
 void InitModels() {
-	mdl_ball = LoadModelFromMesh(GenMeshSphere(1.0f, 32, 32)); mdl_ball.materials[0].shader = shader_lighting;
+	mdl_ball = LoadModelFromMesh(GenMeshSphere(1.0f, 32, 32)); mdl_ball.materials[0].shader = *shaders[current_shader];
 }
 void UnloadModels() {
 	UnloadModel(mdl_ball);
