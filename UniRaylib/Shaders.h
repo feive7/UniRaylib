33305f718b45f59326d546986e41e167a72e754d
feive7 <feive7@gmail.com>
2025-07-29 11:17:35 -0700

    rearranged shaders

--- UniRaylib/Shaders.h
+++ UniRaylib/Shaders.h
@@ -1,6 +1,7 @@
+Shader shader_window;
+
 Shader shader_none;
 Shader shader_darkness;
-Shader shader_window;
 Shader shader_lighting;
 
 int current_shader = 0;
@@ -8,13 +9,16 @@ Shader* shaders[] = {&shader_none,&shader_lighting,&shader_darkness};
 std::string shader_names[] = {"None","Lighting","Test"};
 const int shader_count = sizeof(shaders) / sizeof(Shader*);
 void InitShaders() {
+	shader_window = LoadShader("assets/shaders/window.vs", "assets/shaders/window.fs");
+
 	shader_none = LoadShader("assets/shaders/base.vs", "assets/shaders/base.fs");
 	shader_darkness = LoadShader("assets/shaders/darkness.vs", "assets/shaders/darkness.fs");
-	shader_window = LoadShader("assets/shaders/window.vs", "assets/shaders/window.fs");
 	shader_lighting = LoadShader("assets/shaders/lighting.vs", "assets/shaders/lighting2.fs");
 }
 void UnloadShaders() {
-	UnloadShader(shader_darkness);
 	UnloadShader(shader_window);
+
+	UnloadShader(shader_none);
+	UnloadShader(shader_darkness);
 	UnloadShader(shader_lighting);
 }
