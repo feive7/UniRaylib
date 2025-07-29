fdede6f584bbacdb720ca680e1b2d3fba954f5f7
feive7 <feive7@gmail.com>
2025-07-29 10:41:49 -0700

    Added test shader

--- UniRaylib/Shaders.h
+++ UniRaylib/Shaders.h
@@ -3,12 +3,12 @@ Shader shader_window;
 Shader shader_lighting;
 
 int current_shader = 0;
-Shader* shaders[] = {&shader_lighting};
+Shader* shaders[] = {&shader_lighting,&shader_test};
 const int shader_count = sizeof(shaders) / sizeof(Shader*);
 void InitShaders() {
 	shader_test = LoadShader("assets/shaders/test.vs", "assets/shaders/test.fs");
 	shader_window = LoadShader("assets/shaders/window.vs", "assets/shaders/window.fs");
-	shader_lighting = LoadShader("assets/shaders/lighting.vs", "assets/shaders/lighting1.fs");
+	shader_lighting = LoadShader("assets/shaders/lighting.vs", "assets/shaders/lighting2.fs");
 }
 void UnloadShaders() {
 	UnloadShader(shader_test);
